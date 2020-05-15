f65261e05fc7ca25095b301b9edfdd2c1282c9a9
Jayantajit Gogoi <jayanta.gogoi525@gmail.com>
2020-05-15 14:37:46 +0000

    rk3328: add support for radxa-rockpi-e
    Signed-off-by: Jayantajit Gogoi <jayanta.gogoi525@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -4,13 +4,22 @@
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
+define Device/radxa_rockpi-e
+  DEVICE_VENDOR := Radxa
+  DEVICE_MODEL := RockPi E
+  SOC := rk3328
+  UBOOT_DEVICE_NAME := rockpi-e-rk3328
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | rockchip64-img | gzip | append-metadata
+  DEVICE_PACKAGES += kmod-drm-rockchip kmod-drm-rockchip-hdmi-sound
+endef
+TARGET_DEVICES += radxa_rockpi-e
 
 define Device/pine64_rock64
   DEVICE_VENDOR := Pine64
   DEVICE_MODEL := Rock64
   SOC := rk3328
   UBOOT_DEVICE_NAME := rock64-rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | rockchip64-img | gzip | append-metadata
   DEVICE_PACKAGES += kmod-drm-rockchip kmod-drm-rockchip-hdmi-sound
 endef
 TARGET_DEVICES += pine64_rock64
@@ -20,6 +29,6 @@ define Device/pine64_rockpro64
   DEVICE_MODEL := RockPro64
   SOC := rk3399
   UBOOT_DEVICE_NAME := rockpro64-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | rockchip64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += pine64_rockpro64
